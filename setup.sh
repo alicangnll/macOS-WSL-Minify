c39fe39d43e83c9abaaab0c729e66f608cea51bd
Ali Can Gönüllü <alicangonullu@yahoo.com>
2024-10-19 12:33:27 +0300

    macOS Setup SH
    macOS Setup SH

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cd WSL2-Linux-Kernel-linux-msft-wsl-5.15.153.1/
 cp Microsoft/config-wsl .config
 make menuconfig
 sudo apt-get install qemu-system qemu-utils python3 python3-pip -y  # for Ubuntu, Debian, Mint, and PopOS.
-(ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 ../macOS.qcow2 100G
+(ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 macOS.qcow2 100G
 ./fetch-macOS-v2.py
 (ls RecoveryImage.dmg >> /dev/null 2>&1 && mv RecoveryImage.dmg BaseSystem.dmg) || echo ""
 qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
